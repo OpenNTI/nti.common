86b90b3f0a3f58559e8c5810281e651542e0eddb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-03 15:59:24 +0000

    document
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@58743 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,53 +4,50 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-    'console_scripts': [
-    ],
+	'console_scripts': [
+	],
 }
 
-import platform
-py_impl = getattr(platform, 'python_implementation', lambda: None)
-IS_PYPY = py_impl() == 'PyPy'
-
 TESTS_REQUIRE = [
-    'nose',
-    'nose2[coverage_plugin]',
-    'nose-timer',
-    'nose-progressive',
+	'nose',
     'nose-pudb',
-    'pyhamcrest',
-    'zope.testing',
-    'nti.testing',
-    'nti.nose_traceback_info',
+	'nose-timer',
+	'nose-progressive',
+    'nose2[coverage_plugin]',
+	'pyhamcrest',
+	'zope.testing'
 ]
 
 setup(
-    name='nti.common',
-    version=VERSION,
-    author='Jason Madden',
-    author_email='jason@nextthought.com',
-    description="NTI Store",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='utils',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name='nti.common',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI Store",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='utils',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-    ],
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: Implementation :: PyPy'
+	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti'],
-    tests_require=TESTS_REQUIRE,
+	tests_require=TESTS_REQUIRE,
 	install_requires=[
 		'setuptools',
-        'zope.cachedescriptors',
-        'zope.dottedname'
+		'zope.cachedescriptors',
+		'zope.dottedname'
 	],
-    dependency_links=[],
+	dependency_links=[],
 	entry_points=entry_points
 )

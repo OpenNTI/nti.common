fad8703c95577ba944532702bd23bb0f43e582f0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-28 01:13:42 +0000

    add helper from pyramid
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@60502 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/_compat.py
+++ src/nti/common/_compat.py
@@ -34,6 +34,24 @@ else:
 	text_type = unicode
 	binary_type = str
 
+if PY3: # pragma: no cover
+	def native_(s, encoding='latin-1', errors='strict'):
+		"""
+		If ``s`` is an instance of ``text_type``, return
+		``s``, otherwise return ``str(s, encoding, errors)``
+		"""
+		if isinstance(s, text_type):
+			return s
+		return str(s, encoding, errors)
+else:
+	def native_(s, encoding='latin-1', errors='strict'):
+		""" 
+		If ``s`` is an instance of ``text_type``, return
+		``s.encode(encoding, errors)``, otherwise return ``str(s)``
+		"""
+		if isinstance(s, text_type):
+			return s.encode(encoding, errors)
+		return str(s)
 try:
 	INT_MAX = sys.maxsize
 except AttributeError:

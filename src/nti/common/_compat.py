06c6787378e945f9e9c0c3c7136bb1897fe86b28
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-28 01:11:17 +0000

    add some compact vars
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@60501 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/_compat.py
+++ src/nti/common/_compat.py
@@ -16,9 +16,23 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import sys
+import types
 import struct
 
 PY3 = sys.version_info[0] == 3
+
+if PY3: # pragma: no cover
+	string_types = str,
+	integer_types = int,
+	class_types = type,
+	text_type = str
+	binary_type = bytes
+else:
+	string_types = basestring,
+	integer_types = (int, long)
+	class_types = (type, types.ClassType)
+	text_type = unicode
+	binary_type = str
 
 try:
 	INT_MAX = sys.maxsize

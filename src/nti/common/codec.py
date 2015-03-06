516f873b48e219317975968467d00abe0c4f5bad
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-06 03:59:02 +0000

    add two more common modules
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@61010 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/codec.py
+++ src/nti/common/codec.py
@@ -29,3 +29,20 @@ def hex_encode(raw_bytes):
 						type(raw_bytes).__name__)
 	result = binascii.b2a_hex(raw_bytes)
 	return result
+
+def base64_encode(raw_bytes):
+	"""
+	Encodes raw bytes into base64 representation without appending a trailing
+	newline character. Not URL-safe.
+	
+	Encode your Unicode strings to a byte encoding before base64-encoding them.
+	
+	:param raw_bytes:
+		Bytes to encode.
+	:returns:
+		Base64 encoded bytes without newline characters.
+	"""
+	if not builtins.is_bytes(raw_bytes):
+		raise TypeError("argument must be bytes: got %r" %
+						type(raw_bytes).__name__)
+	return binascii.b2a_base64(raw_bytes)[:-1]

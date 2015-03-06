bcda91d53480c6fc1e2ac5c158b235240dc60e37
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-06 02:57:06 +0000

    alias
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@61000 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/string.py
+++ src/nti/common/string.py
@@ -26,3 +26,4 @@ FALSE_VALUES = ('0', 'n', 'no', 'f', 'false')
 def safestr(s):
     s = s.decode("utf-8") if isinstance(s, bytes) else s
     return unicode(s) if s is not None else None
+to_unicode=safestr

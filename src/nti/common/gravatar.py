633e335cf4436fdf74715cb8df188081f845a0ab
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-10 22:26:36 +0000

    set default size
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@68714 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/gravatar.py
+++ src/nti/common/gravatar.py
@@ -13,20 +13,21 @@ import hashlib
 import urlparse
 
 _AVATAR_SERVICES = {'gravatar': { True: b'secure.gravatar.com',
-								   False: b'www.gravatar.com' },
+								  False: b'www.gravatar.com' },
 					'libravatar': {True: b'seccdn.libravatar.org',
 								   False: b'cdn.libravatar.org'} }
 
 GENERATED_GRAVATAR_TYPES = ('retro', 'identicon', 'monsterid', 'wavatar')  # : Constants identifying the various generated gravatar types
 KNOWN_GRAVATAR_TYPES = GENERATED_GRAVATAR_TYPES + ('mm', '404')  # : Constants for all types of gravatars, generated or static
 
-MYSTERY_MAN_URL = "https://www.gravatar.com/avatar/0?d=mm"
+DEFAULT_SIZE = 128
+MYSTERY_MAN_URL = "https://www.gravatar.com/avatar/0?d=mm&s=%s" % DEFAULT_SIZE
 
 def create_gravatar_url(username,
-						 defaultGravatarType='mm',
-						 secure=False,
-						 size=128,
-						 service='gravatar'):
+						defaultGravatarType='mm',
+						secure=False,
+						size=DEFAULT_SIZE,
+						service='gravatar'):
 	"""
 	Return a gravatar URL for the given username (which is assumed to be an email address).
 
@@ -38,11 +39,11 @@ def create_gravatar_url(username,
 	:return: A gravatar URL for the given username. See http://en.gravatar.com/site/implement/images/
 	"""
 	md5str = hashlib.md5(username.lower()).hexdigest()
-
 	scheme = b'https' if secure else b'http'
 	netloc = _AVATAR_SERVICES[service][secure]
 	path = b'/avatar/' + md5str
 	params = ''
 	query = b's=%s&d=%s' % (size, defaultGravatarType)
 	fragment = ''
-	return str(urlparse.urlunparse((scheme, netloc, path, params, query, fragment)))
+	result = str(urlparse.urlunparse((scheme, netloc, path, params, query, fragment)))
+	return result

8dac54372103774043eeeb6f1d32f88950692174
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-06-08 21:52:51 +0000

    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@90159 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/file.py
+++ src/nti/common/file.py
@@ -11,5 +11,13 @@ logger = __import__('logging').getLogger(__name__)
 
 import re
 
+_nameFinder = re.compile(r'(.*[\\/:])?(.+)')
+
+def name_finder(filename):
+	match = _nameFinder.match(filename) if filename else None
+	result = match.group(2) if match else None
+	return result
+nameFinder = name_finder
+
 def safe_filename(s):
 	return re.sub(r'[/<>:"\\|?*]+', '_', s) if s else s

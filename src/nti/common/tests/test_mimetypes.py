97f5fa30639121f2e99b82b110f5d1a1b11392a5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-06-30 02:12:55 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@91552 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/tests/test_mimetypes.py
+++ src/nti/common/tests/test_mimetypes.py
@@ -23,3 +23,12 @@ class TestMimeTypes(unittest.TestCase):
 		assert_that(t[0], is_('text/mathml'))
 		assert_that(guess_type(None), is_((None, None)))
 
+	def test_image_types(self):
+		assert_that(guess_type('foo.png')[0], is_('image/png'))
+		assert_that(guess_type('foo.jpg')[0], is_('image/jpeg'))
+		assert_that(guess_type('foo.jpeg')[0], is_('image/jpeg'))
+		assert_that(guess_type('foo.bmp')[0], is_('image/bmp'))
+		assert_that(guess_type('foo.gif')[0], is_('image/gif'))
+		assert_that(guess_type('foo.tif')[0], is_('image/tiff'))
+		assert_that(guess_type('foo.tiff')[0], is_('image/tiff'))
+		assert_that(guess_type('foo.jp2')[0], is_('image/jp2'))

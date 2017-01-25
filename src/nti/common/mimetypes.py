5906ed37b9342da8af27e3a87954698af37ed048
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-25 17:36:39 +0000

    remove mimetypes loading
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@105186 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/mimetypes.py
+++ src/nti/common/mimetypes.py
@@ -9,26 +9,12 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import os
-import csv
-
 import mimetypes as p_mimetypes
 
-def _add_local_types():
-	path = os.path.join(os.path.dirname(__file__), "mimetypes.csv")
-	with open(path, "rU") as fp:
-		reader = csv.reader(fp)
-		for row in reader:
-			mimeType = row[0]
-			for ext in [e.strip().lower() for e in row[2].split(',')]:
-				ext = '.' + ext if not ext.startswith('.') else ext
-				p_mimetypes.add_type(mimeType, ext)
-
-_add_local_types()
-del _add_local_types
 
 def guess_type(url, strict=True):
-	return p_mimetypes.guess_type(url, strict) if url else (None, None)
+    return p_mimetypes.guess_type(url, strict) if url else (None, None)
+
 
 def guess_extension(name, strict=True):
-	return p_mimetypes.guess_extension(name, strict)
+    return p_mimetypes.guess_extension(name, strict)

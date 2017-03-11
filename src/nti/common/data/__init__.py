c6510cd0983f291e0f9e1ca916e624e9713d8edf
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-11 00:49:32 +0000

    remove test and move emoji source file to a folder
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@108620 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/common/data/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from nti.common.gravatar import _AVATAR_SERVICES
+from nti.common.gravatar import KNOWN_GRAVATAR_TYPES
+from nti.common.gravatar import GENERATED_GRAVATAR_TYPES
+
+from nti.common.gravatar import create_gravatar_url

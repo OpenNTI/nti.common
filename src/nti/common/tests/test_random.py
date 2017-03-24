5cc3490b4965e0177015f1d367f45d80a945a54b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-24 03:31:30 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@109541 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/common/tests/test_random.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import has_length
+from hamcrest import assert_that
+
+import unittest
+
+from nti.common.random import generate_random_bits
+from nti.common.random import generate_random_string
+from nti.common.random import generate_random_password
+
+
+class TestRandom(unittest.TestCase):
+
+    def test_generate_random_bits(self):
+        assert_that(generate_random_bits(15), has_length(2))
+
+    def test_generate_random_password(self):
+        assert_that(generate_random_password(), has_length(10))
+
+    def test_generate_random_string(self):
+        assert_that(generate_random_string(15), has_length(15))

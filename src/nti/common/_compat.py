fedd6a9dcddd434d8f5dd5715cc427cd84ae7fe4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-30 15:04:55 +0000

    no need to have two versions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@109925 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/_compat.py
+++ src/nti/common/_compat.py
@@ -28,27 +28,17 @@ integer_types = six.integer_types
 
 if PY3:  # pragma: no cover
     _unicode = lambda x: x
-
-    def native_(s, encoding='utf-8', errors='strict'):
-        """
-        If ``s`` is an instance of ``text_type``, return
-        ``s``, otherwise return ``str(s, encoding, errors)``
-        """
-        if isinstance(s, text_type):
-            return s
-        return str(s, encoding, errors)
-
 else:
     _unicode = unicode
 
-    def native_(s, encoding='utf-8', errors='strict'):
-        """
-        If ``s`` is an instance of ``text_type``, return
-        ``s.encode(encoding, errors)``, otherwise return ``str(s)``
-        """
-        if isinstance(s, text_type):
-            return s.encode(encoding, errors)
-        return str(s)
+def native_(s, encoding='utf-8', errors='strict'):
+    """
+    If ``s`` is an instance of ``text_type``, return
+    ``s.encode(encoding, errors)``, otherwise return ``str(s)``
+    """
+    if isinstance(s, text_type):
+        return s.encode(encoding, errors)
+    return str(s)
 
 
 def unicode_(s, encoding='utf-8', err='strict'):

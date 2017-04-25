276c62719ee8e72d56c655165754c4452294f98a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-25 02:29:48 +0000

    add support files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@111305 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ README.rst
@@ -0,0 +1,10 @@
+============
+ nti.common
+============
+
+.. image:: https://travis-ci.org/NextThought/nti.common.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.common
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.common/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.common?branch=master
+
--- setup.py
+++ setup.py
@@ -1,59 +1,61 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
-
 entry_points = {
-	'console_scripts': [
-	],
+    'console_scripts': [
+    ],
 }
 
 TESTS_REQUIRE = [
-	'nose',
-	'nose-pudb',
-	'nose-timer',
-	'nose-progressive',
-	'nose2[coverage_plugin]',
-	'pyhamcrest'
+    'nti.testing',
+    'zope.testrunner',
 ]
 
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
-	name='nti.common',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI common utils",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='common utils',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: Implementation :: CPython',
-		'Programming Language :: Python :: Implementation :: PyPy'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	tests_require=TESTS_REQUIRE,
-	install_requires=[
+    name='nti.common',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI common utilities",
+    long_description=(_read('README.rst')  + '\n\n' + _read('CHANGES.rst')),
+    license='Apache',
+    keywords='common utils',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
 		'setuptools',
 		'emoji',
+		'nameparser',
 		'six',
 		'zope.deprecation',
 		'zope.deferredimport',
 		'zope.dottedname',
 		'zope.interface'
-	],
-	extras_require={
-		'test': TESTS_REQUIRE,
-	},
-	dependency_links=[],
-	entry_points=entry_points
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points,
+    test_suite="nti.common.tests",
 )

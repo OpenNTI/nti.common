a5c525357df36c5afc2f1176f7628bcdd3d6a9f4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-12 21:44:10 +0000

    use common meta directives
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@121590 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/tests/test_zcml.py
+++ src/nti/common/tests/test_zcml.py
@@ -16,21 +16,29 @@ from nti.testing.matchers import verifiably_provides
 
 from zope import component
 
+from nti.common.interfaces import ILDAP
 from nti.common.interfaces import IAWSKey
+from nti.common.interfaces import IOAuthKeys
 
 import nti.testing.base
 
-AWS_ZCML_STRING = u"""
+HEAD_ZCML_STRING = u"""
 <configure xmlns="http://namespaces.zope.org/zope"
 	xmlns:zcml="http://namespaces.zope.org/zcml"
 	xmlns:aws="http://nextthought.com/ntp/aws"
+	xmlns:ldap="http://nextthought.com/ntp/ldap"
+	xmlns:oauth="http://nextthought.com/ntp/oauth"
 	i18n_domain='nti.dataserver'>
 
 	<include package="zope.component" />
+	<include package="zope.annotation" />
 	<include package="z3c.baseregistry" file="meta.zcml" />
 	
 	<include package="." file="meta.zcml" />
 
+"""
+
+AWS_ZCML_STRING = HEAD_ZCML_STRING + u"""
 	<aws:registerAWSKey
 		purpose="S3"
 		grant="private-read-write"
@@ -40,21 +48,58 @@ AWS_ZCML_STRING = u"""
 </configure>
 """
 
+LDAP_ZCML_STRING = HEAD_ZCML_STRING + u"""
+	<ldap:registerLDAP
+		id="nti-ldap"
+		url="ldaps://ldaps.nextthought.com:636"
+		username="jason.madden@nextthougt.com"
+		password="NTI%26123"
+		encoding="urlquote"
+		baseDN="OU=Accounts" />
+</configure>
+"""
+
+OAUTHKEYS_ZCML_STRING = HEAD_ZCML_STRING + u"""
+	<oauth:registerOAuthKeys
+		apiKey="abcd1234"
+		secretKey="efgh5678" />
+</configure>
+"""
+
 
 class TestZcml(nti.testing.base.ConfiguringTestBase):
 
-	def test_aws_registration(self):
-		self.configure_string(AWS_ZCML_STRING)
-		awskey = component.queryUtility(IAWSKey, "S3")
-		assert_that(awskey, is_not(none()))
-		assert_that(awskey, verifiably_provides(IAWSKey))
-		assert_that(awskey, 
-					has_property('Purpose', "S3"))
-		assert_that(awskey, 
-					has_property('Grant', "private-read-write"))
-		assert_that(awskey, 
-					has_property('BucketName', "nti-dataserver-dev"))
-		assert_that(awskey, 
-					has_property('PublicAccessKey', "AKIAIYVGOCPVO6AQRILQ"))
-		assert_that(awskey, 
-					has_property('SecretAccessKey', "aws_s3_secret_access_key"))
+    def test_aws_registration(self):
+        self.configure_string(AWS_ZCML_STRING)
+        awskey = component.queryUtility(IAWSKey, "S3")
+        assert_that(awskey, is_not(none()))
+        assert_that(awskey, verifiably_provides(IAWSKey))
+        assert_that(awskey,
+                    has_property('Purpose', "S3"))
+        assert_that(awskey,
+                    has_property('Grant', "private-read-write"))
+        assert_that(awskey,
+                    has_property('BucketName', "nti-dataserver-dev"))
+        assert_that(awskey,
+                    has_property('PublicAccessKey', "AKIAIYVGOCPVO6AQRILQ"))
+        assert_that(awskey,
+                    has_property('SecretAccessKey', "aws_s3_secret_access_key"))
+
+    def test_ldap_registration(self):
+        self.configure_string(LDAP_ZCML_STRING)
+        ldap = component.queryUtility(ILDAP, name="nti-ldap")
+        assert_that(ldap, is_not(none()))
+        assert_that(ldap, verifiably_provides(ILDAP))
+        assert_that(ldap, 
+					has_property('URL', "ldaps://ldaps.nextthought.com:636"))
+        assert_that(ldap, 
+					has_property('Username', "jason.madden@nextthougt.com"))
+        assert_that(ldap, has_property('Password', "NTI&123"))
+        assert_that(ldap, has_property('BaseDN', "OU=Accounts"))
+
+    def test_oauth_registration(self):
+        self.configure_string(OAUTHKEYS_ZCML_STRING)
+        keys = component.getUtility(IOAuthKeys)
+        assert_that(keys, verifiably_provides(IOAuthKeys))
+        assert_that(keys, has_property('APIKey', "abcd1234"))
+        assert_that(keys, has_property('SecretKey', "efgh5678"))

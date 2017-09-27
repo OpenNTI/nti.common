df9191b1b0e5221c47f45bb2bb6f5a75fc3a0227
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-27 18:23:51 +0000

    copy code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@122409 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/_compat.py
+++ src/nti/common/_compat.py
@@ -18,15 +18,11 @@ from zope import deferredimport
 
 def text_(s, encoding='utf-8', err='strict'):
     """
-    Return a string and unicode version of an object. 
-    If the object is an byte sequence it's decoded first
-    
-    :param object s: The object to get an unicode representation of.
-    :param str encoding: The encoding to be used if ``s`` is a byte sequence
-    :param str err: The err handling scheme to be used if ``s`` is a byte sequence
+    Decode a byte sequence and unicode result
     """
-    s = s.decode(encoding, err) if isinstance(s, bytes) else s
-    return six.text_type(s) if s is not None else None
+    if not isinstance(s, six.text_type) and s is not None:
+        s = s.decode(encoding, err)
+    return s
 
 
 def bytes_(s, encoding='utf-8', errors='strict'):

58f4db5743776e56bb7eeded7eb49e408feae87f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-03 04:53:03 +0000

    100%
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.common/trunk@122660 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/common/tests/test_random.py
+++ src/nti/common/tests/test_random.py
@@ -16,18 +16,46 @@ import unittest
 from nti.common.random import generate_random_bits
 from nti.common.random import generate_random_string
 from nti.common.random import generate_random_password
+from nti.common.random import generate_random_sequence
 from nti.common.random import generate_random_hex_string
+from nti.common.random import generate_random_uint_atmost
+from nti.common.random import generate_random_uint_between
+
 
 class TestRandom(unittest.TestCase):
 
     def test_generate_random_bits(self):
         assert_that(generate_random_bits(15), has_length(2))
+        with self.assertRaises(TypeError):
+            generate_random_bits(3.5)
+        with self.assertRaises(ValueError):
+            generate_random_bits(0)
+
+    def test_generate_random_uint_atmost(self):
+        with self.assertRaises(TypeError):
+            generate_random_uint_atmost(3.5)
+        with self.assertRaises(ValueError):
+            generate_random_uint_atmost(0)
+
+    def test_generate_random_uint_between(self):
+        with self.assertRaises(TypeError):
+            generate_random_uint_between(3.5, 6)
+        with self.assertRaises(ValueError):
+            generate_random_uint_between(3, 3)
+
+    def test_generate_random_sequence(self):
+        with self.assertRaises(TypeError):
+            generate_random_sequence(3.5, ())
+        with self.assertRaises(ValueError):
+            generate_random_sequence(0, ())
 
     def test_generate_random_password(self):
         assert_that(generate_random_password(), has_length(10))
 
     def test_generate_random_string(self):
         assert_that(generate_random_string(15), has_length(15))
-    
+
     def test_generate_random_hex_string(self):
         assert_that(generate_random_hex_string(8), has_length(8))
+        with self.assertRaises(ValueError):
+            generate_random_hex_string(-1)

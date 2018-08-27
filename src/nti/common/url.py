fbf7ea65766f5936a40439a6de2f4fbdb2ca8939
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-27 14:34:12 -0500

    minor

--- src/nti/common/url.py
+++ src/nti/common/url.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
+from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import division
 
 from six.moves import urllib_parse
 
@@ -13,6 +16,7 @@ logger = __import__('logging').getLogger(__name__)
 def safe_add_query_params(url, params):
     """
     Adds query params properly to a url
+
     :param url: The url to be updated
     :param params: The query params in a dictionary
     :return: The url with the query params safely added

0769d8ed95d32337ad8e7b456565d6a635c42d08
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-21 17:12:28 -0500

    adjust signature per's JM input

--- src/nti/common/zcml.py
+++ src/nti/common/zcml.py
@@ -168,10 +168,10 @@ def patch_in_debugger(v, item):
 @interface.implementer(IConfigurationContext, IWithDebugger)
 class WithDebugger(GroupingContextDecorator):
 
-    def __getattr__(self, item, **kw):  # pylint: disable=arguments-differ
-        result = super(WithDebugger, self).__getattr__(item, **kw)
+    def __getattr__(self, name):  # pylint: disable=signature-differs
+        result = super(WithDebugger, self).__getattr__(name)
         if self.context.hasFeature("devmode"):
-            result = patch_in_debugger(result, item)
+            result = patch_in_debugger(result, name)
         else:
             logger.warning(u'A ZCML debugger has been left in %s', self.info)
         return result
